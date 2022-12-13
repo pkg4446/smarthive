40e727e6cc2fc1e083b49ea006313e7b77903bdf
pkg4446 <pkg4446@gmail.com>
2022-12-13 12:40:28 +0900

    221213

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     username: process.env.Database_username,
     password: process.env.Database_password,
     database: "smarthive",
-    host: "192.168.0.28",
+    host: "127.0.0.1",
     dialect: "mysql",
     logging: false
   },
