de6f875a678c77f8cf9d790c7042c4dcc03f6ffa
pkg4446 <pkg4446@gmail.com>
2022-12-19 11:17:52 +0900

    221219

--- routes/demo.js
+++ routes/demo.js
@@ -9,11 +9,21 @@ const log_wh_door   = require('../models/log_wh_door');
 router.get('/O3', async function(req, res) {   
     try {
         const response = await log_wh_O3.findAll({limit:100,  order: [['IDX', 'desc']]});
-        res.render('read/demo',{data:response});
+        res.render('read/demo_O3',{data:response});
     } catch (error) {
         res.sendStatus(404);
     }
 });
+
+router.get('/DOOR', async function(req, res) {   
+    try {
+        const response = await log_wh_door.findAll({limit:100,  order: [['IDX', 'desc']]});
+        res.render('read/demo_Door',{data:response});
+    } catch (error) {
+        res.sendStatus(404);
+    }
+});
+
 
     
 module.exports = router;
