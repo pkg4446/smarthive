15da5d917dece79c123e2a21d5fe5af657292a29
pkg4446 <pkg4446@gmail.com>
2023-04-03 14:48:56 +0900

    230403

--- routes/api/costom_multi.js
+++ routes/api/costom_multi.js
@@ -10,7 +10,6 @@ router.post('/log', async function(req, res) {
         data:   null
     }    
     try {
-        console.log(req.body);
         if(req.body.MODULE){            
             req.body.FARM = requestIp.getClientIp(req);
             response.data = await multi.regist(req.body);
@@ -23,60 +22,15 @@ router.post('/log', async function(req, res) {
     return res.json(response);
 });
 
-router.post('/hiveSensorName', async function(req, res) {    
-    const response = {
-        result: true,
-        data:   null
-    }
-    try {
-        if(req.body.NAME) response.data = await update.sensor_name(req.body);
-    } catch (error) {
-        console.error(error);
-        response.result = false;
-    }
-    return res.json(response);
-});
-
-router.post('/hiveSensor', async function(req, res) {    
-    const response = {
-        result: true,
-        data:   null
-    }
-    try {
-        response.data = await update.sensor_set(req.body);
-    } catch (error) {
-        console.error(error);
-        response.result = false;
-    }
-    return res.json(response);
-});
-
-router.post('/farm', async function(req, res) {
+router.post('/read', async function(req, res) {   
     const response = {
         result: true,
         data:   null
     }    
-    console.log(req.body);
     try {
-        if(req.body.TYPE == "DELETE"){   response.data  = await update.farm_update(req.body.FARM,"APIARY",0);}
-        else if(req.body.TYPE == "NAME"){response.data  = await update.farm_update(req.body.FARM,"NAME",req.body.NAME);}
-    } catch (error) {
-        console.error(error);
-        response.result = false;
-    }
-    return res.json(response);
-});
-
-router.post('/warehouse', async function(req, res) {
-    const response = {
-        result: true,
-        data:   null
-    }    
-    console.log(req.body);
-    try {
-        if(req.body.TYPE == "MQTT"){ await mqtt.send({TARGET:req.body.MODULE, COMMEND:req.body.COMMEND});}
-        else if(req.body.TYPE == "DELETE"){response.data     = await update.warehouse_update(req.body.MODULE,"APIARY",0);}
-        else if(req.body.TYPE == "NAME"){response.data  = await update.warehouse_update(req.body.MODULE,"NAME",req.body.NAME);}
+        if(req.body.MODULE && req.body.START && req.body.END){            
+            response.data = await multi.read(req.body);
+        }else{response.result = false; response.data = "dataNull";}
     } catch (error) {
         console.error(error);
         response.result = false;
