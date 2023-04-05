23e754d596b79f089f624ca942e27bf4fbaf4dc7
pkg4446 <pkg4446@gmail.com>
2023-04-05 12:45:43 +0900

    230405

--- routes/front/page_custom.js
+++ routes/front/page_custom.js
@@ -2,7 +2,7 @@ const express   = require('express');
 const router    = express.Router();
 
 router.get('/multi', async function(req, res) {    
-    res.render('custom/test');
+    res.render('custom/multi');
 });
 
 module.exports = router;
