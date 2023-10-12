930380784dd8f307b70cdbb2d4d317dee64af1d3
pkg4446 <pkg4446@gmail.com>
2023-10-12 16:40:40 +0900

    231012

--- routes/api/update.js
+++ routes/api/update.js
@@ -40,13 +40,25 @@ router.post('/hiveSensor', async function(req, res) {
     }
     try {
         response.data = await update.sensor_set(req.body);
-        console.log(response.data.USE,req.body.USE);
-        console.log(response.data.SET_TEMP,req.body.TEMP);
-        console.log(response.data.SET_HUMI,req.body.HUMI);
-        console.log(req.body);
         if(response.data.USE      != req.body.USE){await mqtt.send({TARGET:response.data.FARM, COMMEND:`;S=${req.body.MODULE}=AT+USE=${req.body.USE};`}); console.log("mqtt1");}
         if(response.data.SET_TEMP != req.body.TEMP){await mqtt.send({TARGET:response.data.FARM, COMMEND:`;S=${req.body.MODULE}=AT+TEMP=${req.body.TEMP};`}); console.log("mqtt2");}
         if(response.data.SET_HUMI != req.body.HUMI){await mqtt.send({TARGET:response.data.FARM, COMMEND:`;S=${req.body.MODULE}=AT+HUMI=${req.body.HUMI};`}); console.log("mqtt3");}
+    } catch (error) {
+        console.error(error);
+        response.result = false;
+    }
+    return res.json(response);
+});
+
+router.post('/hiveFind', async function(req, res) {    
+    const response = {
+        result: true,
+        data:   null
+    }
+    try {
+        response.data = await update.sensor_set(req.body);
+        await mqtt.send({TARGET:response.data.FARM, COMMEND:`;S=${req.body.MODULE}=AT+SHOW=${req.body.LED};`}); 
+        console.log("mqtt1");
     } catch (error) {
         console.error(error);
         response.result = false;
